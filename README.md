be4bcf493af12ac903fb7b30bc801a5c41b93e6e
MarcioPorto <mporto@macalester.edu>
2017-12-06 03:24:12 -0700

    Added helper methods.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Spark Movie Recommender
 
 This is a simple script for a movie recommender system using Apache Spark.
+
+## Download data
+
+## Run the script
+
+On Hadoop:
+
+spark-submit recommender.py
+
+On local computer:
+
+python recommender.py
--- recommender.py
+++ recommender.py
@@ -4,8 +4,29 @@ from pyspark.sql import Row
 from pyspark.sql.functions import lit
 
 
+def load_movie_names():
+  """ Loads a dictionary of movie_ids as keys and the corresponding movie_name
+      as the value.
+  """
+  movie_names = dict()
+  with open("ml-100k/u.item") as f:
+    for line in f:
+      fields = line.split("|")
+      movie_names[int(fields[0])] = fields[1].decode("ascii", "ignore")
+  return movie_names
+
+def parse_input(line):
+  """ Transforms a line of data into a Row object.
+  """
+  fields = line.value.split()
+  return Row(
+    user_id   = int(fields[0]),
+    movie_id  = int(fields[1]),
+    rating    = int(fields[2])
+  )
+
 if __name__ == "__main__":
   spark = SparkSession.builder.appName("MovieRecommendations").getOrCreate()
-  
+
   # Kill Spark session
   spark.stop()
